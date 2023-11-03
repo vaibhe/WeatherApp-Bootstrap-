421f4c3042b9be751bcd6bed8814db1c5dd9425d
Vaibhe <vaibhavambati28@gmail.com>
2023-11-03 21:33:14 +0530

    added unixconverter func

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ const options = {
 	}
 };
 
+function formatUnixTimestamp(unix_timestamp) {
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds
+    var date = new Date(unix_timestamp * 1000);
+  
+    // Hours part from the timestamp
+    var hours = date.getHours();
+  
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+  
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+  
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  
+    return formattedTime;
+  }
+
 const getWeather = (city) =>{
 	cityName.innerHTML = city
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
@@ -16,15 +36,17 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, op
 	.then((response) => {
 	console.log(response)
 
-	temp.innerHTML= response.temp
-	feels_like.innerHTML= response.feels_like
-	humidity.innerHTML= response.humidity
-	min_temp.innerHTML= response.min_temp
-	max_temp.innerHTML= response.max_temp
-	wind_speed.innerHTML= response.wind_speed
-	wind_degrees.innerHTML= response.wind_degrees
-	sunrise.innerHTML= response.sunrise
-	sunset.innerHTML= response.sunset
+	temp.innerHTML= response.temp + " °C"
+	feels_like.innerHTML= response.feels_like + " °C"
+	humidity.innerHTML= response.humidity + " %"
+	min_temp.innerHTML= response.min_temp + " °C"
+	max_temp.innerHTML= response.max_temp + " °C"
+	wind_speed.innerHTML= response.wind_speed + " km/hr"
+	wind_degrees.innerHTML= response.wind_degrees + " °C"
+	var sunrisetime =  formatUnixTimestamp(response.sunrise)
+	var sunsettime =   formatUnixTimestamp(response.sunset)
+	sunrise.innerHTML= sunrisetime + " am"
+	sunset.innerHTML=  sunsettime + " pm"
 	
 })
 	
